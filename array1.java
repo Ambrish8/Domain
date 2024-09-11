fe1297860209f7e7332e723ceda8763e9b26867c
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-09-11 20:32:10 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,11 @@
+public class array1
+{
+    public static void main(String args[])
+    {
+        int nums[] = {2,1,3,5};
+        for(int i=0;i<4;i++)
+        {
+            System.out.print(nums[i]+ " ");
+        }
+    }
+}
