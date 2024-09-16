42942a81430160e9f29a044951011f1305a05a12
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-09-16 16:03:26 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,areaS;
+    printf("The side of a Square is ");
+    scanf("%d",&a);
+    void area();
+    {
+        areaS=a*a;
+        printf("The area of a square is %d ",areaS);
+    }
+    return areaS;
+}
