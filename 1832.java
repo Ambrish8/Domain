8a06259c0c5f6e01924f88c763ab999c4924af90
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-10-08 01:32:43 +0530

    Create 1832.java

---
+++ 1832.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] arr = new boolean[26];
+        for(int i=0;i<sentence.length();i++)
+        {
+            arr[sentence.charAt(i)-97]=true;
+        }
+        for(int j=0;j<26;j++)
+        {
+            if(arr[j]==false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
