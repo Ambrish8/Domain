487d63a69179f79646e464ab321e3a7ddf126916
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-10-10 03:00:33 +0530

    Update 832.java

--- 832.java
+++ 832.java
@@ -4,7 +4,7 @@ class Solution {
         int n=image[0].length;
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<(n+1)/2;j++)
+            for(int j=0;j<(n+1)/2;j++)    //no. of columns will be fixed cuz there is only 3 column and 0th and 2nd column are to be changed..
             {
                 int temp=image[i][j]^1;
                 image[i][j]=image[i][n-j-1]^1;
