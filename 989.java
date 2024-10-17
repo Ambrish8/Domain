b2c1a19fada3a63e5db0e8c0c6bae46d84efe93a
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-10-18 01:12:43 +0530

    Create 989.java

---
+++ 989.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        int n=num.length;
+        int i=n-1;
+        List<Integer> ans = new ArrayList<>();
+        while(i>=0 || k>0)
+        {
+            if(i>=0)
+            {
+                ans.add((num[i]+k)%10);
+                k=((num[i]+k)/10);
+            }
+            else
+            {
+                ans.add(k%10);
+                k/=10;
+            }
+            i--;
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
