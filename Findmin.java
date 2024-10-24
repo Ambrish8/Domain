9629affd41aa4833a314a1f8deaf4f5c798480c6
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-10-25 02:00:30 +0530

    Add files via upload

---
+++ Findmin.java
@@ -0,0 +1,20 @@
+public class Findmin
+{
+    public static void main(String[] args)
+    {
+        int[] arr={18,12,-7,3,14,28};
+        System.out.println(min(arr));
+    }
+    static int min(int[] arr)
+    {
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+        }
+        return min;
+    }
+}
