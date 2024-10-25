158c7f2fb978f8a2483302d2c3fac3a79fec0310
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-10-26 02:58:37 +0530

    Add files via upload

---
+++ ambrish.java
@@ -0,0 +1,11 @@
+class ambrish
+{
+     public statuc void main(String[] args)
+     {
+      System.out.println("Hello world");
+     }
+}
+class sachin
+{
+     System.out.println("I am sachin");
+}
