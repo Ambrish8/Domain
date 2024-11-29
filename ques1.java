010086ad26010b3a022cd0a73d45d8191b09e3a3
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-11-29 14:49:13 +0530

    Add files via upload

---
+++ ques1.java
@@ -0,0 +1,10 @@
+public class ques1 {
+     public static void main(String[] args) {
+       System.out.println("AMBRISH KALIA 2230143");
+       System.out.println("Name");
+       System.out.println("Roll number");
+       System.out.println("Branch");
+       System.out.println("Department");
+       System.out.println("University");
+   }
+}
