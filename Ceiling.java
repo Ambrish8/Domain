2c2744490674fcef4466b5c0eb6db6e1c1a70a5d
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-12-03 04:30:09 +0530

    Add files via upload

---
+++ Ceiling.java
@@ -0,0 +1,36 @@
+public class Ceiling
+{
+    public static void main(String args[])
+    {
+        int[] arr={-28,-23,-18,-12,0,3,5,7,34,66,78,99};
+        int target=34;
+        int ans=ceiling(arr,target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr, int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        if(target>arr[end])
+        {
+            return -1;
+        }
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(target<arr[mid])
+            {
+                end=mid-1;
+            }
+            else if(target>arr[mid])
+            {
+                start=mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
