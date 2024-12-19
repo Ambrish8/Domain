9f6630ead58f2871f60e5edc4bf5f8c07d1c7bab
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2024-12-19 11:48:58 +0530

    Add files via upload

---
+++ kate.java
@@ -0,0 +1,14 @@
+public class kate
+{
+    public static void main(String args[])
+
+    {
+        int count=0;
+        while(count<11)
+        {
+            count=count +1;
+            System.out.println(count);
+
+        }
+    }
+}
