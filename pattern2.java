b92464db69bcefe847469f9dc6b1c59346d26557
Ambrish Kalia <117847499+Ambrish8@users.noreply.github.com>
2025-01-16 18:28:41 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,18 @@
+public class pattern2
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1;i < 7;i++)
+        {
+            for(int k=4;k>1;k--)
+            {
+                System.out.print();
+            }
+            for(int j = 1; j < = i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
